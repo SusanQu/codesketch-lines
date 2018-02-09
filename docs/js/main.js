63d6695b044241af28269e575ef1891874f43a09
SuzieQQQ <susan@susanqu.com>
2018-02-08 20:05:06 -0500

    adjusting color and opacity

--- docs/js/main.js
+++ docs/js/main.js
@@ -3,7 +3,8 @@
 var scene, camera, renderer, controls, aspect, clock;
 var graph, material, mesh, line, particles, particle, randomLine;
 
-var colors = [0x50514f, 0xf0b67f, 0xf25f5c, 0x70c1b3];
+//0.dark gray, 1.orange, 2.red, 3.teal, 4.white, 5.dark blue
+var colors = [0x50514f, 0xf0b67f, 0xf25f5c, 0x70c1b3, 0xe9e9e9, 0x324668];
 var frustumSize = 500;
 
 
@@ -22,18 +23,9 @@ function init() {
     //camera.position.z = 100;
     camera.position.set( 50, 50, 0 );
 
-    // var cameraOrthoHelper = new THREE.CameraHelper( camera );
-		// scene.add( cameraOrthoHelper );
-
-
-    graph = new THREE.Object3D();
-    scene.add(graph);
-
-
     //particles will be the 3D object containing all the particles
     particles = new THREE.Object3D();
     scene.add(particles);
-
 
     // Grid
 		// var gridHelper = new THREE.GridHelper( 1000, 20 );
@@ -50,34 +42,32 @@ function init() {
 
 }
 
-
-function makeLine (geo, color, width, opacity){
-
-    line = new MeshLine();
-    line.setGeometry( geo );
-
-    material = new MeshLineMaterial({
-      color: new THREE.Color( colors[ color ] ),
-      lineWidth: width,
-      opacity: opacity,
-      transparent: true
-    });
-
-    mesh = new THREE.Mesh( line.geometry, material );
-
-    graph.add( mesh );
-}
+//the makeLine() function is embedded in createParticle()
+// function makeLine (geo, color, width, opacity){
+//
+//     line = new MeshLine();
+//     line.setGeometry( geo );
+//
+//     material = new MeshLineMaterial({
+//       color: new THREE.Color( colors[ color ] ),
+//       lineWidth: width,
+//       opacity: opacity,
+//       transparent: true
+//     });
+//
+//     mesh = new THREE.Mesh( line.geometry, material );
+//
+//     graph.add( mesh );
+// }
 
 
 function createParticle(){
 
 	//Create a geometry used for the particles which contains nothing for now
 	var geometry = new THREE.Geometry();
-	var vertices0 = new THREE.Vector3(0,
-                                    (Math.random()-0.5)*100,
-                                    (20-Math.random()*40))
-	var vertices1 = new THREE.Vector3(vertices0.x+6, vertices0.y, vertices0.z);
-	// var vertices3 = new THREE.Vector3(12,17,15);
+	var vertices0 = new THREE.Vector3(0, (Math.random()-0.5)*100, (20-Math.random()*40));
+	var vertices1 = new THREE.Vector3(vertices0.x+5, vertices0.y, vertices0.z);
+
 	//apply our vector inside the geometry
 	geometry.vertices.push(vertices0, vertices1);
 
@@ -85,16 +75,16 @@ function createParticle(){
   particle.setGeometry( geometry );
 
   material = new MeshLineMaterial({
-    color: new THREE.Color( colors[ 1 ] ),
-    lineWidth: 0.5,
-    opacity: 1,
+    color: new THREE.Color( colors[5] ),
+    lineWidth: Math.random() * 0.35,
+    opacity: Math.random() * 0.85,
     transparent: true
   });
 
   particle = new THREE.Mesh(particle.geometry, material);
 
 	//create a random speed for each particle for aesthetics
-	particle.speed = 0.02+ (Math.random()-0.5)/1000;
+	particle.speed = 0.03+ (Math.random()-0.5)/1000;
 
 	//We set a random position for each particle
 	particle.direction = {
@@ -142,17 +132,12 @@ function render(){
 		//Get the next particle
 		var particle = particles.children[i];
 
-		//We move our particle closer to its destination
+		//move particle closer to its destination, need to use position, not vertices
 		particle.position.x += particle.direction.x * particle.speed;
 		particle.position.y += particle.direction.y  * particle.speed;
 		particle.position.z += particle.direction.z * particle.speed;
 
-    //We move our particle closer to its destination
-    // particle.geometry.vertices[1].x += particle.direction.x * particle.speed;
-    // particle.geometry.vertices[1].y += particle.direction.y * particle.speed;
-    // particle.geometry.vertices[1].z += particle.direction.z * particle.speed;
-
-    //We reduce the opacity of the particle
+    //reduce the opacity of the particle
 		particle.material.opacity -= 0.005;
 
     //Prevents ThreeJs the particle has moved
@@ -160,7 +145,7 @@ function render(){
 
 		//If the opacity of the particle is too low
 		if(particle.material.opacity < 0.05){
-			//We remove our particle from the scene
+			//remove our particle from the scene
 			particles.remove(particle);
 			//The loop must go through the same 'i' because we removed one particle from the array
 			i--;
